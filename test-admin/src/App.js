d9863d611898d6a8bbdefd529931f42c849bc272
easwar1213 <35298840+easwar1213@users.noreply.github.com>
2018-11-04 15:39:24 +0530

    L-13 - Add Login Page & Authentication Provider

--- test-admin/src/App.js
+++ test-admin/src/App.js
@@ -6,11 +6,12 @@ import { PostList, PostEdit, PostCreate } from './posts';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 import Dashboard from './Dashboard';
+import authProvider from './authProvider';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => (
-    <Admin dashboard={Dashboard} dataProvider={dataProvider}>        
+    <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>        
         <Resource name="users" list={UserList} icon={UserIcon}/>
         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
     </Admin>
