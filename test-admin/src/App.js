824ad34a37207ecee51bfa2dfb32c475e70ebb9e
easwar1213 <35298840+easwar1213@users.noreply.github.com>
2018-11-04 17:14:00 +0530

    L-15-Add Data Provider

--- test-admin/src/App.js
+++ test-admin/src/App.js
@@ -7,8 +7,9 @@ import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 import Dashboard from './Dashboard';
 import authProvider from './authProvider';
+import dataProvider from './dataProvider';
 
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+//const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => (
     <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>        
