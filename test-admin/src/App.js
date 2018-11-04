1424cb04cdb2167eda2cabdff8d2f4b7cc36019d
easwar1213 <35298840+easwar1213@users.noreply.github.com>
2018-11-04 14:13:15 +0530

    L-3-Render React Admin with Child Resource Users

--- test-admin/src/App.js
+++ test-admin/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
-import { Admin, Resource } from 'react-admin';
+import { Admin } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import { Resource,ListGuesser } from 'react-admin';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
-const App = () => <Admin dataProvider={dataProvider} />;
+
+const App = () => (
+    <Admin dataProvider={dataProvider}>
+        <Resource name="users" list={ListGuesser} />
+    </Admin>
+);
 
 export default App;
